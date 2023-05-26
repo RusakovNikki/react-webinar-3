e7ae1185b3921db776ae787c9dac2f596576b738
RusakovNikki <rusakov.nikki@yandex.ru>
2023-05-27 01:57:00 +0300

    сделал закрытие модалки при выборе в ней элемента

--- src/components/item-basket/index.js
+++ src/components/item-basket/index.js
@@ -4,19 +4,24 @@ import {numberFormat} from "../../utils";
 import {cn as bem} from "@bem-react/classname";
 import PropTypes from "prop-types";
 import './style.css';
+import {Link} from 'react-router-dom';
 
 function ItemBasket(props) {
 
   const cn = bem('ItemBasket');
 
   const callbacks = {
-    onRemove: (e) => props.onRemove(props.item._id)
+    onRemove: (e) => props.onRemove(props.item._id),
+    onSelect: () => props.closeModal()
   };
 
   return (
     <div className={cn()}>
-      {/*<div className={cn('code')}>{props.item._id}</div>*/}
-      <div className={cn('title')}>{props.item.title}</div>
+      <div className={cn('title')}>
+        <Link to={`/about/${props.item._id}`} onClick={callbacks.onSelect}>
+          {props.item.title}
+        </Link>
+      </div>
       <div className={cn('right')}>
         <div className={cn('cell')}>{numberFormat(props.item.price)} ₽</div>
         <div className={cn('cell')}>{numberFormat(props.item.amount || 0)} шт</div>
@@ -37,7 +42,7 @@ ItemBasket.propTypes = {
 }
 
 ItemBasket.defaultProps = {
-  onRemove: () => {},
+  onRemove: () => { },
 }
 
 export default memo(ItemBasket);
