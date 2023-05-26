54fbc77b6618337d5f8f4b0d4cbb358f72601474
RusakovNikki <rusakov.nikki@yandex.ru>
2023-05-26 23:43:47 +0300

    линки в AboutPage

--- src/app/main/pages/about-page/index.jsx
+++ src/app/main/pages/about-page/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { cn as bem } from "@bem-react/classname";
+import { Link } from "react-router-dom";
+import "./style.css";
 
 const AboutPage = () => {
-  return <>wenfwhiwne</>;
+  const cn = bem("AboutPage");
+
+  return (
+    <div className={cn()}>
+      <Link to="/">Главная</Link>
+    </div>
+  );
 };
 
 export default AboutPage;
