f0f83ed5c16d3724f00f6751ce66d16387815dc9
RusakovNikki <rusakov.nikki@yandex.ru>
2023-05-27 02:46:19 +0300

    доделал добавление при обновлении страницы AboutPage

--- src/store/item/index.js
+++ src/store/item/index.js
@@ -22,14 +22,20 @@ class Item extends StoreModule {
     }
 
     async load(id) {
-        const response = await fetch(`/api/v1/articles/${id}?fields=*,madeIn(title,code),category(title)`);
-        const json = await response.json();
+        const json = await this.fetchItem(id)
 
         console.log(json);
         this.setState({
             ...this.getState(),
             data: {...json.result}
         }, 'Загружены товары из АПИ');
+    }
+
+    async fetchItem(id) {
+        const response = await fetch(`/api/v1/articles/${id}?fields=*,madeIn(title,code),category(title)`);
+        const json = await response.json();
+
+        return json
     }
 }
 
